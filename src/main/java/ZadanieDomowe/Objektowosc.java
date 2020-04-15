e4eca64067b87342254ab1fd6c54c1e35ffc3da5
Ciechmar <Ciechmar@o2.pl>
2020-04-15 20:36:12 +0200

    Obiektowość zad z odcinkami

--- src/main/java/ZadanieDomowe/Objektowosc.java
+++ src/main/java/ZadanieDomowe/Objektowosc.java
@@ -1,25 +1,36 @@
 package ZadanieDomowe;
 
 
-import static java.lang.StrictMath.pow;
+import static java.lang.StrictMath.*;
 
 public class Objektowosc {
     public static void main(String[] args) {
-        Odcinek odcinek = new Odcinek(2, 2, 4,5);
+        Odcinek odcinek = new Odcinek(10, 6, 4,5);
         double x = odcinek.x;
         double y= odcinek.y;
         double x2 = odcinek.x2;
         double y2 = odcinek. y2;
 
        double dlugosc = dlugoscODcinak(x,y,x2,y2);
-        System.out.printf("Długość tego odcinka to: %.2f", dlugosc);
+        System.out.printf("Długość tego odcinka to: %.2f\n", dlugosc);
+        rysujOdcinek(dlugosc);
 
     }
 
     public static double dlugoscODcinak (double x, double y, double x2, double y2){
         double dlugosc;
         dlugosc = Math.sqrt(pow((x2-x),2)+ pow((y2-y),2));
-        return dlugosc;
+        return abs(dlugosc);
+    }
+
+    public static void rysujOdcinek (double dlugosc){
+        int dlugoscInt = (int) dlugosc;
+        System.out.print("|");
+        for (int i = 0; i <dlugoscInt ; i++) {
+            System.out.print("-");
+        }
+        System.out.print("|");
+
     }
 
 
